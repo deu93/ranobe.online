ec637d9957a66c2d76382854caf229a6693f471f
deu93 <deucalion93@ukr.net>
2021-11-19 13:05:46 +0200

    2

---
+++ database/migrations/2021_11_19_092457_create_books_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('image')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('description');
+            $table->text('about');
+            $table->string('slug');
+            $table->string('author');
+            $table->string('country');
+            $table->integer('likes');
+            $table->integer('dislikes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}

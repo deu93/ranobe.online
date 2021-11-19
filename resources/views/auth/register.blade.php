455fb45caabdd274bc9c8349ad08df403299b97c
Deu <deucalion93@ukr.net>
2021-11-19 18:51:46 +0200

    2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,27 +2,52 @@
 
 @section('content')
 <div class="login_page">
-    <div class="login_block">
+    <div class="register_block">
         <h2>Создайте аккаунт</h2>
         <form action="{{ url('register') }}" method="POST">
+            @csrf
             <div class="auth_item">
-                <input type="text" id="name" placeholder="Ваше имя">
+                <input type="text" id="name" class="@error('name') error
+                @enderror" placeholder="Ваше имя">
+            @error('name')
+                <div class="error_text">
+                    <h4>Введите корректные данные!</h4>
+                </div>
+            @enderror
             </div>
             <div class="auth_item">
-                <input type="text" id="username" placeholder="Логин">
+                <input type="text" class="@error('username') error
+                @enderror" id="username" placeholder="Логин">
+                @error('username')
+                <div class="error_text">
+                    <h4>Введите корректные данные!</h4>
+                </div>
+            @enderror
             </div>
 
             <div class="auth_item">
-                <input type="email" id="email" placeholder="Email">
+                <input type="email" class="@error('email') error
+                @enderror" id="email" placeholder="Email">
+                @error('email')
+                <div class="error_text">
+                    <h4>Введите корректные данные!</h4>
+                </div>
+            @enderror
             </div>
             <div class="auth_item">
-                <input type="password" id="password" placeholder="Введите пароль">
+                <input type="password" class="@error('password') error
+                @enderror" id="password" placeholder="Введите пароль">
+                @error('password')
+                <div class="error_text">
+                    <h4>Введите корректные данные!</h4>
+                </div>
+                @enderror
             </div>
             <div class="auth_item">
                 <input type="password" id="password_confirmation" placeholder="Подтвердите пароль">
             </div>
-            <div class="auth_item_button">
-                <button type="submit">Войти</button>
+            <div class="reg_item_button">
+                <button type="submit">Зарегистрироваться</button>
             </div>
         </form>
     </div>

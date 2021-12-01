7e923207c8eaee42d5d22778dd6ee53b62a933ac
deu93 <deucalion93@ukr.net>
2021-12-01 11:13:06 +0200

    upd

--- resources/views/description.blade.php
+++ resources/views/description.blade.php
@@ -166,13 +166,15 @@
             <div class="hearder_chapters_list">
                 Список глав
             </div>
-           <div id="scroll_bar" class="chapter_block">
-           @foreach ($chapters as $chapter )
-               <div class="chapter">
-                    <a href="{{url('/'.$book->slug.'/'.$chapter->slug)}}">{{ $chapter->chapter_name }}</a>
+           <div class="scroll_reverse">
+            <div id="scroll_bar" class="chapter_block">
+                @foreach ($chapters as $chapter )
+                    <div class="chapter">
+                         <a href="{{url('/'.$book->slug.'/'.$chapter->slug)}}">{{ $chapter->chapter_name }}</a>
+                     </div>
+                @endforeach
+                 
                 </div>
-           @endforeach
-            
            </div>
         </div>
         

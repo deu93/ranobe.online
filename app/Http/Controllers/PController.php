92cd80b72c24a3f0c842f0ffb7453d4000956564
Deu <deucalion93@ukr.net>
2021-12-12 17:57:10 +0200

    upd

--- app/Http/Controllers/PController.php
+++ app/Http/Controllers/PController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class PController extends Controller
 {
     public function index() {
-        $debil = Chapter::all();
+        $debil = Chapter::where('book_id', 27)->get();
         foreach ($debil as $debilchik) {
             $debilchik->chapter_text .= ">" ;
         }

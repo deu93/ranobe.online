6960a9476cbdcf2e5fa50f9abee785485beaf23c
deu93 <deucalion93@ukr.net>
2021-12-15 09:35:13 +0200

    upd

--- app/Http/Controllers/DescriptionController.php
+++ app/Http/Controllers/DescriptionController.php
@@ -35,29 +35,61 @@ class DescriptionController extends Controller
             }
         }
         
+        function is_bot()
+        {
+            if (!empty($_SERVER['HTTP_USER_AGENT'])) {
+                $options = array(
+                    'YandexBot', 'YandexAccessibilityBot', 'YandexMobileBot','YandexDirectDyn',
+                    'YandexScreenshotBot', 'YandexImages', 'YandexVideo', 'YandexVideoParser',
+                    'YandexMedia', 'YandexBlogs', 'YandexFavicons', 'YandexWebmaster',
+                    'YandexPagechecker', 'YandexImageResizer','YandexAdNet', 'YandexDirect',
+                    'YaDirectFetcher', 'YandexCalendar', 'YandexSitelinks', 'YandexMetrika',
+                    'YandexNews', 'YandexNewslinks', 'YandexCatalog', 'YandexAntivirus',
+                    'YandexMarket', 'YandexVertis', 'YandexForDomain', 'YandexSpravBot',
+                    'YandexSearchShop', 'YandexMedianaBot', 'YandexOntoDB', 'YandexOntoDBAPI',
+                    'Googlebot', 'Googlebot-Image', 'Mediapartners-Google', 'AdsBot-Google',
+                    'Mail.RU_Bot', 'bingbot', 'Accoona', 'ia_archiver', 'Ask Jeeves', 
+                    'OmniExplorer_Bot', 'W3C_Validator', 'WebAlta', 'YahooFeedSeeker', 'Yahoo!',
+                    'Ezooms', '', 'Tourlentabot', 'MJ12bot', 'AhrefsBot', 'SearchBot', 'SiteStatus', 
+                    'Nigma.ru', 'Baiduspider', 'Statsbot', 'SISTRIX', 'AcoonBot', 'findlinks', 
+                    'proximic', 'OpenindexSpider','statdom.ru', 'Exabot', 'Spider', 'SeznamBot', 
+                    'oBot', 'C-T bot', 'Updownerbot', 'Snoopy', 'heritrix', 'Yeti',
+                    'DomainVader', 'DCPbot', 'PaperLiBot'
+                );
         
-        $visitor_ip = $_SERVER['REMOTE_ADDR'];
-        $visited = UserIp::where('ip_address', $visitor_ip)->where('book_id', $book->id)->first();
-        $book_view = BookView::where('book_id', $book->id)->first();
-        
-        if($visited == null) {
-            $visited = new UserIp();
-            $visited->ip_address = $visitor_ip;
-            $visited->book_id = $book->id;
-            $visited->save();
-
-            
-            if($book_view == null) {
-                $book_view = new BookView();
-                $book_view->book_id = $book->id;
-                $book_view->views = 1;
-                $book_view->save();
+                foreach($options as $row) {
+                    if (stripos($_SERVER['HTTP_USER_AGENT'], $row) !== false) {
+                        return true;
+                    }
+                }
             }
-            else if ($book_view != null){
+        
+            return false;
+        }
+        if (is_bot() == false) {
+            $visitor_ip = $_SERVER['REMOTE_ADDR'];
+            $visited = UserIp::where('ip_address', $visitor_ip)->where('book_id', $book->id)->first();
+            $book_view = BookView::where('book_id', $book->id)->first();
+            
+            if($visited == null) {
+                $visited = new UserIp();
+                $visited->ip_address = $visitor_ip;
+                $visited->book_id = $book->id;
+                $visited->save();
+
                 
-                $book_view->views = $book_view->views + 1;
-                
-                $book_view->update();
+                if($book_view == null) {
+                    $book_view = new BookView();
+                    $book_view->book_id = $book->id;
+                    $book_view->views = 1;
+                    $book_view->save();
+                }
+                else if ($book_view != null){
+                    
+                    $book_view->views = $book_view->views + 1;
+                    
+                    $book_view->update();
+                }
             }
         }
         
--- app/Http/Controllers/ReaderController.php
+++ app/Http/Controllers/ReaderController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\DB;
 
 class ReaderController extends Controller
 {
+    
     public function index($book_slug, $chapter_slug) {
         $book = Book::where('slug', $book_slug)->first();
         $chapter = Chapter::where('slug', $chapter_slug)->first();
@@ -24,29 +25,61 @@ class ReaderController extends Controller
         ->where('id', '<', $chapter->id)
         ->latest()
         ->first();
+        function is_bot()
+        {
+            if (!empty($_SERVER['HTTP_USER_AGENT'])) {
+                $options = array(
+                    'YandexBot', 'YandexAccessibilityBot', 'YandexMobileBot','YandexDirectDyn',
+                    'YandexScreenshotBot', 'YandexImages', 'YandexVideo', 'YandexVideoParser',
+                    'YandexMedia', 'YandexBlogs', 'YandexFavicons', 'YandexWebmaster',
+                    'YandexPagechecker', 'YandexImageResizer','YandexAdNet', 'YandexDirect',
+                    'YaDirectFetcher', 'YandexCalendar', 'YandexSitelinks', 'YandexMetrika',
+                    'YandexNews', 'YandexNewslinks', 'YandexCatalog', 'YandexAntivirus',
+                    'YandexMarket', 'YandexVertis', 'YandexForDomain', 'YandexSpravBot',
+                    'YandexSearchShop', 'YandexMedianaBot', 'YandexOntoDB', 'YandexOntoDBAPI',
+                    'Googlebot', 'Googlebot-Image', 'Mediapartners-Google', 'AdsBot-Google',
+                    'Mail.RU_Bot', 'bingbot', 'Accoona', 'ia_archiver', 'Ask Jeeves', 
+                    'OmniExplorer_Bot', 'W3C_Validator', 'WebAlta', 'YahooFeedSeeker', 'Yahoo!',
+                    'Ezooms', '', 'Tourlentabot', 'MJ12bot', 'AhrefsBot', 'SearchBot', 'SiteStatus', 
+                    'Nigma.ru', 'Baiduspider', 'Statsbot', 'SISTRIX', 'AcoonBot', 'findlinks', 
+                    'proximic', 'OpenindexSpider','statdom.ru', 'Exabot', 'Spider', 'SeznamBot', 
+                    'oBot', 'C-T bot', 'Updownerbot', 'Snoopy', 'heritrix', 'Yeti',
+                    'DomainVader', 'DCPbot', 'PaperLiBot'
+                );
         
-        $visitor_ip = $_SERVER['REMOTE_ADDR'];
-        $visited = UserChapterIp::where('ip_address', $visitor_ip)->where('chapter_id', $chapter->id)->first();
-        $chapter_view = ChapterView::where('chapter_id', $chapter->id)->first();
-        
-        if($visited == null) {
-            $visited = new UserChapterIp();
-            $visited->ip_address = $visitor_ip;
-            $visited->chapter_id = $chapter->id;
-            $visited->save();
-
-            
-            if($chapter_view == null) {
-                $chapter_view = new ChapterView();
-                $chapter_view->chapter_id = $chapter->id;
-                $chapter_view->views = 1;
-                $chapter_view->save();
+                foreach($options as $row) {
+                    if (stripos($_SERVER['HTTP_USER_AGENT'], $row) !== false) {
+                        return true;
+                    }
+                }
             }
-            else if ($chapter_view != null){
+        
+            return false;
+        }
+        if(is_bot() == false) {
+            $visitor_ip = $_SERVER['REMOTE_ADDR'];
+            $visited = UserChapterIp::where('ip_address', $visitor_ip)->where('chapter_id', $chapter->id)->first();
+            $chapter_view = ChapterView::where('chapter_id', $chapter->id)->first();
+            
+            if($visited == null) {
+                $visited = new UserChapterIp();
+                $visited->ip_address = $visitor_ip;
+                $visited->chapter_id = $chapter->id;
+                $visited->save();
+
                 
-                $chapter_view->views = $chapter_view->views + 1;
-                
-                $chapter_view->update();
+                if($chapter_view == null) {
+                    $chapter_view = new ChapterView();
+                    $chapter_view->chapter_id = $chapter->id;
+                    $chapter_view->views = 1;
+                    $chapter_view->save();
+                }
+                else if ($chapter_view != null){
+                    
+                    $chapter_view->views = $chapter_view->views + 1;
+                    
+                    $chapter_view->update();
+                }
             }
         }
         

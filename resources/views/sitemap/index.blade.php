faa645a5bcf3ca9f9845b2a78fb236ae17d5b606
deu93 <deucalion93@ukr.net>
2021-12-15 09:42:49 +0200

    upd

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -7,4 +7,22 @@
     <sitemap>
         <loc>{{ url('/stm/sitemap/chapters') }}</loc>
     </sitemap>
+    <url>
+        <loc>http://ranobe.online/</loc>
+      </url>
+      <url>
+        <loc>https://ranobe.online/all-books</loc>
+      </url>
+      <url>
+        <loc>https://ranobe.online/finished-books</loc>
+      </url>
+      <url>
+        <loc>https://ranobe.online/login</loc>
+      </url>
+      <url>
+        <loc>https://ranobe.online/register</loc>
+      </url>
+      <url>
+        <loc>https://ranobe.online/search</loc>
+      </url>
 </sitemapindex>

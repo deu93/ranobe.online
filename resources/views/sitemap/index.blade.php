e413095facaeb1a2925d87c482deaa0d1673399c
deu93 <deucalion93@ukr.net>
2021-12-15 09:46:15 +0200

    upd

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -2,27 +2,29 @@
  
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <sitemap>
-        <loc>{{ url('/stm/sitemap/books') }}</loc>
+        <loc>{{ url('/stm/sitemap/books.xml') }}</loc>
     </sitemap>
     <sitemap>
-        <loc>{{ url('/stm/sitemap/chapters') }}</loc>
+        <loc>{{ url('/stm/sitemap/chapters.xml') }}</loc>
     </sitemap>
-    <url>
-        <loc>http://ranobe.online/</loc>
-      </url>
-      <url>
-        <loc>https://ranobe.online/all-books</loc>
-      </url>
-      <url>
-        <loc>https://ranobe.online/finished-books</loc>
-      </url>
-      <url>
-        <loc>https://ranobe.online/login</loc>
-      </url>
-      <url>
-        <loc>https://ranobe.online/register</loc>
-      </url>
-      <url>
-        <loc>https://ranobe.online/search</loc>
-      </url>
 </sitemapindex>
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"> 
+  <url>
+    <loc>http://ranobe.online/</loc>
+  </url>
+  <url>
+    <loc>https://ranobe.online/all-books</loc>
+  </url>
+  <url>
+    <loc>https://ranobe.online/finished-books</loc>
+  </url>
+  <url>
+    <loc>https://ranobe.online/login</loc>
+  </url>
+  <url>
+    <loc>https://ranobe.online/register</loc>
+  </url>
+  <url>
+    <loc>https://ranobe.online/search</loc>
+  </url>
+</urlset>

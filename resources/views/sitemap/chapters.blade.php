6d16c95e024eb69138113cbd01736cf24a8ea7f2
deu93 <deucalion93@ukr.net>
2021-12-15 10:34:58 +0200

    upd

--- resources/views/sitemap/chapters.blade.php
+++ resources/views/sitemap/chapters.blade.php
@@ -1,9 +1,6 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     @foreach ($chapters as $chapter)
-        @php
-          $book= App\Models\Book::where('id', $chapter->book_id)->first()
-        @endphp
         <url>
             <loc>{{url("/reader/". $chapter->book_id . '/' . $chapter->slug)}}</loc>
         </url>
